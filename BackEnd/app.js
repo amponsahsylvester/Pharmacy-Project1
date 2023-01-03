ce924c66f6a6a8c6b291999a88676428aee869b8
Asamoah_Andrews <57991005+Andrews-Asamoah@users.noreply.github.com>
2023-01-03 18:52:20 +0000

    created a databse with users and drugs table

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,33 +1,54 @@
 const express = require("express");
 const mysql = require("mysql");
 
-// Create connection
+// create connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "1234567",
+  password: "",
+  database: "pharmacy",
 });
 
 // connect
 db.connect((err) => {
-  if (err) {
+  try {
+    console.log("MySql Connected .....");
+  } catch {
     throw err;
   }
-  console.log("MySql Connected....");
-});
-
-// Create DB
-app.get("/createdb", (req, res) => {
-  let sql = "CREATE DATABASE pharmacy";
-  db.query(sql, (err, result) => {
-    if (err) throw err;
-    console.log(result);
-    res.send("Database created successfully ....");
-  });
 });
 
 const app = express();
 
-app.listen("3306", () => {
-  console.log("Server Started on Port 3306");
+// Create DB
+app.get("/pharmacyDb", (req, res) => {
+  let sql = "CREATE DATABASE Pharmacy";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Database created........");
+  });
 });
+
+// Create table
+app.get("/users", (req, res) => {
+  let sql =
+    "CREATE TABLE users (id int AUTO_INCREMENT PRIMARY KEY, FirstName varchar(100), LastName varchar(100),username varchar(100), password varchar(100))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Users table created......");
+  });
+});
+
+app.get("/drugs", (req, res) => {
+  let sql =
+    "CREATE TABLE drugs (id int AUTO_INCREMENT PRIMARY KEY, name varchar(100), price varchar(100),image varchar(100), description varchar(255),category varchar(100))";
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Drugs table created......");
+  });
+});
+
+app.listen("3000", () => {});
